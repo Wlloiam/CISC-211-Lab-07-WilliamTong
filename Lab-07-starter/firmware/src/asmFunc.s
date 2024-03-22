11742372585d6d7387c604681568d6baaeaa2918
Wlloiam <118074610+Wlloiam@users.noreply.github.com>
2024-03-22 13:38:32 -0700

    lab_07_williamtong

--- Lab-07-starter/firmware/src/asmFunc.s
+++ Lab-07-starter/firmware/src/asmFunc.s
@@ -66,7 +66,7 @@ asmFunc:
     mov r10,r0			//storing the r0,input value from C code, in r10
     ldr r1,=constant_value1	//storing 0xffff0000 in r1
     ldr r2,=constant_value2	//storing 0x0000ffff in r2
-    ANDS r5,r10,r1		//using logical operator AND to get the value of A, then update the flags
+    ANDS r5,r10,r1		//using logical operator AND to get the value of A, then update the flags, unpacking
     LSR r5,16			//shifting the value in r5 to right 16 times
     BMI Negative1		//if the negative flag is set, will direct to the negative1 branch
     B Case_For_B		//if the neagtive flag is not set, will direct to the Case_For_B branch
@@ -75,9 +75,9 @@ asmFunc:
 	ORR r5,r5,r1		//using logical operator OR to make sign extension for A value
     /**This is Case_For_B branch. This branch will unpack the input from C code. if the value is negative, will do sign extension, otherwise, not***/
     Case_For_B:
-	AND r6,r10,r2		//using logical operator AND to get the value of B
+	AND r6,r10,r2		//using logical operator AND to get the value of B, unpacking
 	LSL r3,r6,16		//shifting the B value to the left 16 times to check the sign value, and stored it in r3
-	ADDS r3,r3,0		//adding 0 to r3, and store it in r3 to check the sign value of B
+	ADDS r3,r3,0		//adding 0 to r3, and store it in r3, then update the flags to check the sign value of B
 	BMI Negative2		//if the negative flag is set, will direct to the Negative2 branch
 	B Storing_A_and_B	//if the negative flag is not set, will direct to the Storing_A_and_B branch
     /**This is Negative2 branch. This is for the sign extension for B value.**/
@@ -89,8 +89,6 @@ asmFunc:
 	STR r5,[r9]		//storing the A value in a_value
 	LDR r9,=b_value		//storing the memory location of b_value in r9
 	STR r6,[r9]		//storing the B value in b_value
-	
-	
 	
     /*** STUDENTS: Place your code ABOVE this line!!! **************/
 
